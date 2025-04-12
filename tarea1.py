ab0989badb5264d49df97e12e7b16a3857709f99
NoPosGuau <samuel.romney14@gmail.com>
2025-04-12 18:23:22 -0400

    I add Clicking Radio Buttom

--- tarea1.py
+++ tarea1.py
@@ -6,8 +6,7 @@ import time
 driver = webdriver.Chrome()
 driver.get("https://testautomationpractice.blogspot.com")
 
-# Wait for the page to load
-time.sleep(5)
+#enter the name, email, phone number, and text in the textarea
 web_element = driver.find_element(By.ID, "name")
 web_element.send_keys("Samuel Romney")
 time.sleep(2)
@@ -18,5 +17,15 @@ web_element = driver.find_element(By.ID, "phone")
 web_element.send_keys("1234567890")
 web_element = driver.find_element(By.ID, "textarea")
 web_element.send_keys("esto es un nuevo cambio. ")
+
+#click on the button "male" and "monday" and "friday"
+web_element = driver.find_element(By.ID, "male")
+web_element.click()
+
+web_element = driver.find_element(By.ID, "monday")
+web_element.click()
+
+web_element = driver.find_element(By.ID, "friday")
+web_element.click()
 
 time.sleep(15)
