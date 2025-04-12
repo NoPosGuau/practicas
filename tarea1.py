7506c89e3def9cfe7a0640d7573eca191ff4d5e0
NoPosGuau <samuel.romney14@gmail.com>
2025-04-12 18:08:26 -0400

    Prueba 1

---
+++ tarea1.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://testautomationpractice.blogspot.com")
+
+# Wait for the page to load
+time.sleep(5)
+web_element = driver.find_element(By.ID, "name")
+web_element.send_keys("Samuel Romney")
+time.sleep(2)
+web_element = driver.find_element(By.ID, "email")
+web_element.send_keys("SR@TEST.COM")
+time.sleep(2)
+web_element = driver.find_element(By.ID, "phone")
+web_element.send_keys("1234567890")
+
+time.sleep(15)
